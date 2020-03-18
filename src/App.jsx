e00943a33bbeb113ccab55ef233b339747021bfd
denisgat <denisgatotho@gmail.com>
2020-03-18 14:44:05 -0400

    uncomment assignment rendering

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,7 @@ class App extends React.Component {
     let tabChoice = <div />;
 
     /*Uncomment below to render assignments*/
-    /*if (this.state.buttonClicked === "assignments") {
+    if (this.state.buttonClicked === "assignments") {
       tabChoice = (
         <List
           placeholder="Add Assignment..."
@@ -59,20 +59,20 @@ class App extends React.Component {
           title="Assignments"
         />
       );
-    }*/
+    }
 
     /* Change below to render students*/
 
-    /*if (this.state.buttonClicked === "students") {
-      tabChoice = (
-        <List
-          placeholder="Add Assignment..." 
-          currList={this.state.assignments}
-          addFunction={this.addAssignment}
-          title="Student Roster"
-        />
-      );
-    }*/
+    // if (this.state.buttonClicked === "students") {
+    //   tabChoice = (
+    //     <List
+    //       placeholder="Add Assignment..." 
+    //       currList={this.state.assignments}
+    //       addFunction={this.addAssignment}
+    //       title="Student Roster"
+    //     />
+    //   );
+    // }
 
     /* Uncomment lines below to render grades*/
     /*if (this.state.buttonClicked === "grades") {
@@ -84,7 +84,7 @@ class App extends React.Component {
           data={this.state.grades}
         />
       );
-    }*/
+    // }*/
 
     return (
       <div>
